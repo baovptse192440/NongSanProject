34e76d768ed44ee3a1b786e5d8aff9c0d8d78a40
phamkhoa18 <phamkhoa3092003@gmail.com>
2025-11-26 15:00:37 +0700

    fix image

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,30 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  images: {
+    // vì mình serve uploads cùng origin qua Nginx (http://13.213.48.10)
+    // nên KHÔNG cần remotePatterns nếu dùng đường dẫn tương đối (/uploads/...)
+    // Nếu bạn vẫn dùng full URL 13.213.48.10 thì dùng remotePatterns:
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "13.213.48.10",
+        port: "",          // để trống = port 80
+        pathname: "/uploads/**",
+      },
+      // nếu thật sự còn chỗ nào dùng :8888
+      {
+        protocol: "http",
+        hostname: "13.213.48.10",
+        port: "8888",
+        pathname: "/uploads/**",
+      },
+    ],
   },
 };
 
